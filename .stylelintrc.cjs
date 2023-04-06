bf62a8b1b87c2cec1c890b2485ceee367f407d3f
ychesniuk <yurii.chesniuk@titanbay.com>
2023-04-06 13:27:31 +0300

    Added stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-recommended-vue'
+  ],
+
+  rules: {
+    'no-empty-source': null,
+    'at-rule-no-unknown': null,
+    'no-descending-specificity': null,
+    'value-keyword-case': null,
+    'selector-class-pattern': '^[a-z][a-zA-Z0-9_-]+$',
+    'selector-id-pattern': '^[a-z][a-zA-Z0-9_-]+$'
+  }
+}
